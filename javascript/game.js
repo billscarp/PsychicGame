518b0d24473bbb64c38c772fb969338f34bfe1ab
billscarp <billscarp@gmail.com>
2017-08-24 10:19:59 -0400

    New files

---
+++ javascript/game.js
@@ -0,0 +1,23 @@
+//ARRAY//
+
+AZ ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var wins = 0;
+
+var losses = 0;
+
+var guesses = 9;
+
+var guessesSoFar = [];
+
+
+}
+use random Letter //ref rock paper sissors //
+
+use keystroke event //ref rock paper sissors//
+
+
+
+
+
+}
